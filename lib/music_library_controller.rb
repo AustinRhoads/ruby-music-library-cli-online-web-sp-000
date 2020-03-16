4db43880ffc0c3c7a968b9ea00d034b679a98019
Austin Rhoads <austinrrhoads@ymail.com>
2020-03-16 18:59:21 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -33,15 +33,15 @@ def call
       when   'list songs'
         self.list_songs
       when   'list artists'
-        self.list_artists 
+        self.list_artists
       when   'list genres'
-        self.list_genres 
+        self.list_genres
       when   'list artist'
         self.list_songs_by_artist
       when   'list genre'
         self.list_songs_by_genre
-      when   'play song'   
-        self.play_song             
+      when   'play song'
+        self.play_song
       end
     end
 
