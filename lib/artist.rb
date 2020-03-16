64c32906772b473fe37ffbd1b045d74c7cb186d9
Austin Rhoads <austinrrhoads@ymail.com>
2020-03-16 19:04:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,62 +1,4 @@
 require 'pry'
-#class Artist
-
-#extend Concerns::Findable
-
-#@@all = []
-
-#attr_accessor :name
-
-#def initialize(name)
-#@genres = []
-#@songs = []
-#@name = name
-#self.save
-#end
-
-#def songs
-#@songs
-#end
-
-#def add_song(song)
-#if songs.none?(song) == true && song.artist == nil
-##  notes = Song.new(song, self)
-#  @songs << song
-#  song.artist = self
-#end
-#end
-
-#def save
-#@@all << self
-#end
-
-#def genres
-#songs.each do |song|
-#  if song.artist == self
-#    @genres << song.genre
-#  end
-#end
-#@genres.uniq
-#end
-
-#def self.create(name)
-#artist = Artist.new(name)
-#artist
-#end
-
-#def self.all
-#@@all.uniq
-#end
-
-#def self.destroy_all
-#  @@all.clear
-#end
-
-#end
-
-
-
-
 
 class Artist
 
@@ -68,7 +10,6 @@ include Findable
 attr_accessor :name
 
 def initialize(name)
-#@genres = []
 @songs = []
 @name = name
 self.save
@@ -80,16 +21,11 @@ def songs
       @songs << song
     end
   end
-##  @songs.each do |song|if song.genre != nil && # @genres.none?(song.genre)
-  ##  @genres << song.genre
-   ##end
-  ##end
 @songs
 end
 
 def add_song(song)
 if songs.none?(song) == true && song.artist == nil
-#  notes = Song.new(song, self)
   @songs << song
   song.artist = self
 end
@@ -101,15 +37,12 @@ end
 
 def genres
   arr = []
-##self.songs
 self.songs.each do |song|
   if song.genre != nil
     arr << song.genre
   end
 end
 arr.uniq
-#songs.each{|song| @genres << song.genre}
-##@genres.uniq
 end
 
 def self.create(name)
